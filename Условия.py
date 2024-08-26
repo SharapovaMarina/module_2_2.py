0bb00adc747ec321591d8bf9507a60c4c67bdc02
SharapovaMarina <sanitzkaja.mar@yandex.ru>
2024-08-26 18:41:00 +0300

    Add files via upload

---
+++ Условия.py
@@ -0,0 +1,24 @@
+first = 123
+second = 456
+third = 789
+print(first)
+print(second)
+print(third)
+if first == second == third:
+    print(3)
+if first == second or first == third or second == third:
+    print(2)
+else: print(0)
+number1 = 42
+number2 = 69
+number3 = 42
+print(number1)
+print(number2)
+print(number3)
+if number1 == number2 == number3:
+    print(3)
+if number1 == number2 or number1 == number3 or number2 == number3:
+    print(2)
+else: print(0)
+
+
